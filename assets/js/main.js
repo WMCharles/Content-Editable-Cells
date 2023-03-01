06ff3d21b2e59fb8cfee97b2161ea7714d1ed409
wmcharles <charleswafula73@gmail.com>
2023-03-01 21:59:30 +0300

    failed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    // ajax
+    // retrieve data and populate table
     $.ajax({
         type: "GET",
         url: "db.php",
@@ -15,5 +15,40 @@ $(document).ready(function () {
                 tbody.append(tr);
             });
         }
+    });
+
+    $("#editable_table td").click(function () {
+        var td = $(this);
+        var input = $("<input>").val(td.text()).appendTo(td.empty()).focus();
+        input.blur(function () {
+            var newText = $(this).val();
+            if (newText !== "") {
+                td.text(newText);
+                var row = td.closest("tr");
+                var rowData = {
+                    id: row.data("id"),
+                    name: row.find("td:eq(0)").text(),
+                    email: row.find("td:eq(1)").text(),
+                    phone: row.find("td:eq(2)").text()
+                };
+                $.ajax({
+                    type: "POST",
+                    url: "update_data.php",
+                    data: rowData,
+                    success: function (data) {
+                        console.log("Data updated successfully!");
+                    }
+                });
+            }
+            else {
+                td.text(td.data("origText"));
+            }
+        });
+        input.keypress(function (event) {
+            if (event.keyCode === 13) {
+                input.blur();
+            }
+        });
+        td.data("origText", td.text());
     });
 })
