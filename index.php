f437cb9b0b110d50ff8375b85332405d992841e6
wmcharles <charleswafula73@gmail.com>
2023-03-01 21:23:07 +0300

    edit table

--- index.php
+++ index.php
@@ -7,12 +7,25 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" href="./assets/css/style.css">
-  <title>Hello, world!</title>
+  <title>Editable Cells!</title>
 </head>
 
 <body>
   <div class="content mt-4">
-    <h1>These Are My Customers</h1>
+    <h1>Botwa Farm</h1>
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">customerName</th>
+          <th scope="col">phone</th>
+          <th scope="col">addressLine1</th>
+          <th scope="col">creditLimit</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
   </div>
 
   <!-- Optional JavaScript -->
