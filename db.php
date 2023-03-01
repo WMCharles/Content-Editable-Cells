518e7b0fd01791965eaffaa0cc42d5a0b89c31eb
wmcharles <charleswafula73@gmail.com>
2023-03-01 21:15:10 +0300

    initial commit

---
+++ db.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sampledb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve data from the table
+$sql = "SELECT customerNumber, customerName, phone, addressLine1, creditLimit FROM customers";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $data = array();
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    echo json_encode($data);
+} else {
+    echo "0 results";
+}
+
+$conn->close();
