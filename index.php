db61ae28e7fabfdd0213ec55deb84f8701a9b0a2
wmcharles <charleswafula73@gmail.com>
2023-03-02 21:16:13 +0300

    add query

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 require_once('db.php');
+$sql = "SELECT customerNumber, customerName, phone, addressLine1, creditLimit FROM customers LIMIT 12";
+$result = $conn->query($sql);
 ?>
 <!doctype html>
 <html lang="en">
@@ -9,17 +11,29 @@ require_once('db.php');
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.3/css/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" href="./assets/css/style.css">
+
+  <!-- js -->
+  <script defer src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script defer src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
+  <script defer src="https://cdn.datatables.net/1.13.3/js/dataTables.bootstrap4.min.js"></script>
+  <script defer src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script defer src="./assets/js/main.js"></script>
+
+
   <title>Editable Cells!</title>
 </head>
 
 <body>
   <div class="content mt-4">
     <h1>Botwa Farm</h1>
-    <table class="table" id="editable_table">
-      <thead class="thead-dark">
+    <table class="table table-bordered" id="editable_table">
+      <thead>
         <tr>
-          <th scope="col">#</th>
+          <th scope="col">ID</th>
           <th scope="col">customerName</th>
           <th scope="col">phone</th>
           <th scope="col">addressLine1</th>
@@ -45,11 +59,6 @@ require_once('db.php');
       </tbody>
     </table>
   </div>
-
-  <script src="./assets/js/jquery-3.6.3.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  <script src="./assets/js/main.js"></script>
 </body>
 
 </html>
