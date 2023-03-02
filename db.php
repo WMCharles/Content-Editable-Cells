88f7988e3e11ac41e56fb6c458fc67c08030d8d5
wmcharles <charleswafula73@gmail.com>
2023-03-02 21:16:00 +0300

    separate concern

--- db.php
+++ db.php
@@ -12,8 +12,5 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve data from the table
-$sql = "SELECT customerNumber, customerName, phone, addressLine1, creditLimit FROM customers LIMIT 12";
-$result = $conn->query($sql);
 
-$conn->close();
+
